fa5735da231d065165320b0a237f7b68164b5a1b
lucabuonfante <lucabuonfante04@gmail.com>
2026-03-19 19:33:32 +0000

    esercitazione 2 con CMake senza ordinamento

---
+++ esercitazione2/build/CMakeFiles/eseguibileEsempio.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/eseguibileEsempio.dir/link.d"
+  "CMakeFiles/eseguibileEsempio.dir/esempio.cpp.o"
+  "CMakeFiles/eseguibileEsempio.dir/esempio.cpp.o.d"
+  "eseguibileEsempio"
+  "eseguibileEsempio.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/eseguibileEsempio.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ esercitazione2/build/CMakeFiles/eseguibileEsercizio.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/eseguibileEsercizio.dir/link.d"
+  "CMakeFiles/eseguibileEsercizio.dir/esercizio.cpp.o"
+  "CMakeFiles/eseguibileEsercizio.dir/esercizio.cpp.o.d"
+  "eseguibileEsercizio"
+  "eseguibileEsercizio.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/eseguibileEsercizio.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
